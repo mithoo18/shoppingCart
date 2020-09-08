a87bf2859972891b19ff127138ea42144c810941
mithoo <devanshu1g8@gmail.com>
2020-09-08 14:05:17 +0530

    fragment created library added

---
+++ app/src/main/java/com/example/shoppingcart/viewmodels/ShopViewModel.java
@@ -0,0 +1,6 @@
+package com.example.shoppingcart.viewmodels;
+
+import androidx.lifecycle.ViewModel;
+
+public class ShopViewModel extends ViewModel {
+}
--- app/src/main/java/com/example/shoppingcart/views/MainActivity.java
+++ app/src/main/java/com/example/shoppingcart/views/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.shoppingcart;
+package com.example.shoppingcart.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.shoppingcart.R;
 
 public class MainActivity extends AppCompatActivity {
 
